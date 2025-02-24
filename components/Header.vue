f9f00fb42d0a95e59da2d1cca33ec50d172017a7
Rémi Robichet <remi.robichet@edtake.com>
2025-02-24 14:57:28 +0100

    feat: add custom 404 error page and update header spacing

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <NuxtLink class="block my-14" to="/">
+  <NuxtLink class="block py-14" to="/">
     <h1 class="font-heading jersey-20-regular text-center text-8xl">Ctrl + Rems</h1>
     <h2 class="font-heading text-center text-8xl">⌨️</h2>
   </NuxtLink>
---
+++ error.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="py-14">
+    <h1 class="font-heading text-center jersey-20-regular text-8xl">404</h1>
+    <NuxtLink to="/" class="block text-center my-8">
+      <Button class="btn btn-primary">Go back</Button>
+    </NuxtLink>
+  </div>
+  <img src="/img/404.gif" alt="404" class="block mx-auto my-14" />
+</template>
